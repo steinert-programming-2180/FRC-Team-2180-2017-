cca1507178542fa77916d98ef08f59120e229f3f
steinertprogramming <steinertprogramming@gmail.com>
2017-02-10 19:21:38 -0500

    Add files via upload

---
+++ Socket Programming/LiveServer.py
@@ -0,0 +1,38 @@
+'''
+Created on Feb 10, 2017
+
+@author: Steinert FRC2180
+'''
+import socket
+import sys
+
+HOST = ''
+PORT = 5000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
+
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print('Bind failed. Error code: ' + str(msg[0]) + 'Message ' + msg[1])
+    sys.exit()
+    
+print('Socket bind complete')
+
+s.listen(10)
+print('Socket now listening')
+
+while 1:
+    conn, addr = s.accept()
+    print('Connected with ' + addr[0] + ':' + str(addr[1]))
+    
+    data = conn.recv(1024)
+    reply = 'OK... ' + str(data)
+    if not data:
+        break
+    
+    conn.sendall(reply)
+
+conn.close()
+s.close()
